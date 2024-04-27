6d10cc4af342c63388c69f0aca766877a7543858
sabiinsearch <sabiinsearch@gmail.com>
2024-04-27 16:22:21 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,21 @@ feel free to contact
 support me by subscribing to my channel */
 #include <Arduino.h>
 #include <Wire.h>
+
+#define MUX_Address 0x70 // TCA9548A Encoders address
+
+// Initialize I2C buses using TCA9548A I2C Multiplexer
+void tcaselect(uint8_t i2c_bus) {
+    if (i2c_bus > 7) return;
+    Wire.beginTransmission(MUX_Address);
+    Wire.write(1 << i2c_bus);
+    Wire.endTransmission(); 
+}
  
  
 void setup()
 {
-  Wire.begin(0x3C);
+  Wire.begin(MUX_Address);
  
   Serial.begin(9600);
   while (!Serial);             // Leonardo: wait for serial monitor
@@ -22,44 +32,5 @@ void setup()
  
 void loop()
 {
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    // Serial.print(F("checking address: "));
-    // Serial.println(address);
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
+          
 }
